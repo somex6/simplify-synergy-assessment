725196e66ab65235a2a68a77c8b35c77fb7cb576
Iweka somto <iwekasomex61@gmail.com>
2024-11-24 20:20:52 +0100

    Delete simplify-synergy-assessment directory

--- simplify-synergy-assessment/terraform/iam.tf
+++
@@ -1,7 +0,0 @@
-module "iam_users" {
-  source = "./modules/iam"
-
-  name          = var.username
-  path          = var.path
-  force_destroy = var.force_destroy
-}
--- simplify-synergy-assessment/terraform/modules/vm/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "arn" {
-  description = "The ARN of the instance"
-  value       = aws_instance.this.arn
-}
